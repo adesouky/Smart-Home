52df98eed559cfc647099610c424be0e52eb0a62
kimoantiqe <31572141+kimoantiqe@users.noreply.github.com>
2018-04-01 04:27:57 -0700

    Sync across mobile and desktop version , multiple changes and fixes

---
+++ Web 3.0/html/homepage/get_realoadStat_status.php
@@ -0,0 +1,20 @@
+<?php 	
+$host="localhost";
+$user="root";
+$password="Koko2009";
+$db="statusFlags";
+ 
+$get=mysqli_connect($host,$user,$password);
+mysqli_select_db($get,$db);
+$sql="SELECT status FROM `reloadStat` WHERE `id` = 1";
+$ans= mysqli_query($get,$sql);
+
+$data = array();
+foreach ($ans as $row) {
+	$data[] = $row;
+}
+
+//Print data in JSON format
+echo implode($data[0]);
+
+?>
