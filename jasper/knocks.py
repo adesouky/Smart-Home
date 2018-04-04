c49cf79283939be473f7d748b7c5cd4ca43b35a9
sheenayadav <sheenayadav98@gmail.com>
2018-04-04 00:21:19 -0700

    knock testing

---
+++ jasper/knocks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+#GPIO SETUP
+sound = 18
+knocks = 0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sound, GPIO.IN)
+
+def callback(sound):
+        if GPIO.input(sound):
+                print "Knock Detected!"
+		knocks++;
+	
+	if knocks >= 4:
+	
+	
+GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(sound, callback)  # assign function to GPIO PIN, Run function on change
+
+# infinite loop
+while True:
+        time.sleep(1)
