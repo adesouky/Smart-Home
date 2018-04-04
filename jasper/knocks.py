c861c73762088bc7aa47cc423e97605e60ca0119
sheenayadav <sheenayadav98@gmail.com>
2018-04-04 00:55:27 -0700

    knocky

--- jasper/knocks.py
+++ jasper/knocks.py
@@ -9,16 +9,18 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(sound, GPIO.IN)
 
 def callback(sound):
-        
-	global knocks = 0;
 
 	if GPIO.input(sound):
-                print "Knock Detected!"
-		knocks+=1;
-	
-	if knocks >= 4:
-		print "door unlocked"
-	
+                start = time.time()
+		print 'first knock detected'
+		knocks = 0;
+		while time.time()-start < 10:
+			if GPIO.input(sound):
+				knocks+=1;
+				print 'knock detected'
+		if knocks >= 1
+			print 'door unlocked'
+			
 GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(sound, callback)  # assign function to GPIO PIN, Run function on change
 
