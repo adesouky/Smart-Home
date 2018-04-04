b99de98943bf83c983df430107c100d8ab722dd1
sheenayadav <sheenayadav98@gmail.com>
2018-04-04 03:03:50 -0700

    final

---
+++ jasper/knocks_final.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import urllib2
+
+#GPIO SETUP
+sound = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sound, GPIO.IN)
+
+def setOpenDoorFalse():
+    urllib2.urlopen("http://38.88.74.88/homepage/set_openDoor_false.php").read()
+    return
+
+def callback(sound):
+	t=0
+	if GPIO.input(sound):
+		print 'first knock detected'
+		t=1
+		time.sleep(2)
+		start = time.time()
+		
+		while time.time()-start < 3:
+			if GPIO.input(sound):
+				print 'knock 2 detected'
+				t=2
+				break
+		
+		if t!=2:
+			return
+
+		time.sleep(2)
+		
+		start2 = time.time()
+		t2=0
+		while time.time()-start2 < 10:
+			if GPIO.input(sound):
+				print 'knock detected'
+				t2 += 1
+				time.sleep(1)
+			if t2 >= 4:
+				break
+		if t+t2 >= 6:
+			setOpenDoorFalse()			
+			
+GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime=100)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(sound, callback)  # assign function to GPIO PIN, Run function on change
+
+# infinite loop
+while True:
+        time.sleep(1)
